64ce2f80c1dcf9a6107f64dfa4a831ccb2f370ea
Mike Purvis <mikedotexe@gmail.com>
2021-09-07 15:36:58 -0700

    use 4.0.0-pre.2 version, log_str instead

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{near_bindgen, env};
-
-near_sdk::setup_alloc!();
+use near_sdk::{env, near_bindgen};
 
 const PUZZLE_NUMBER: u8 = 1;
 
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
 pub struct Contract {
-    crossword_solution: String
+    crossword_solution: String,
 }
 
 #[near_bindgen]
@@ -23,9 +21,9 @@ impl Contract {
 
     pub fn guess_solution(&mut self, solution: String) {
         if solution == self.crossword_solution {
-            env::log(b"You guessed right!")
+            env::log_str("You guessed right!")
         } else {
-            env::log(b"Try again.")
+            env::log_str("Try again.")
         }
     }
 }
@@ -34,20 +32,19 @@ impl Contract {
  * the rest of this file sets up unit tests
  * to run these, the command will be:
  * cargo test --package rust-template -- --nocapture
- * Note: 'rust-template' comes from cargo.toml's 'name' key
+ * Note: 'rust-template' comes from Cargo.toml's 'name' key
  */
 
 // use the attribute below for unit tests
 #[cfg(test)]
 mod tests {
     use super::*;
-    use near_sdk::{MockedBlockchain, testing_env};
     use near_sdk::test_utils::VMContextBuilder;
-    use near_sdk::json_types::ValidAccountId;
+    use near_sdk::{testing_env, AccountId};
 
     // part of writing unit tests is setting up a mock context
     // provide a `predecessor` here, it'll modify the default context
-    fn get_context(predecessor: ValidAccountId) -> VMContextBuilder {
+    fn get_context(predecessor: AccountId) -> VMContextBuilder {
         let mut builder = VMContextBuilder::new();
         builder.predecessor_account_id(predecessor);
         builder
@@ -61,7 +58,8 @@ mod tests {
         // Using a unit test to rapidly debug and iterate
         let debug_solution = "near nomicon ref finance";
         let debug_hash_bytes = env::sha256(debug_solution.as_bytes());
-        let debug_byte_array: Vec<String> = debug_hash_bytes.iter()
+        let debug_byte_array: Vec<String> = debug_hash_bytes
+            .iter()
             .map(|b| format!("{:02X}", b))
             .collect();
         let debug_hash_string = debug_byte_array.join("");
