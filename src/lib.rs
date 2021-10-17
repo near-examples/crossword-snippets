2069dcfd5419570ba4b54321005f674bb6f84224
Mike Purvis <mikedotexe@gmail.com>
2021-10-17 08:46:25 -0700

    remove other tests, only keeping debug_get_hash

--- src/lib.rs
+++ src/lib.rs
@@ -45,16 +45,8 @@ impl Contract {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use near_sdk::test_utils::{get_logs, VMContextBuilder};
-    use near_sdk::{testing_env, AccountId};
-
-    // part of writing unit tests is setting up a mock context
-    // provide a `predecessor` here, it'll modify the default context
-    fn get_context(predecessor: AccountId) -> VMContextBuilder {
-        let mut builder = VMContextBuilder::new();
-        builder.predecessor_account_id(predecessor);
-        builder
-    }
+    use near_sdk::test_utils::VMContextBuilder;
+    use near_sdk::testing_env;
 
     #[test]
     fn debug_get_hash() {
@@ -66,29 +58,5 @@ mod tests {
         let debug_hash_bytes = env::sha256(debug_solution.as_bytes());
         let debug_hash_string = hex::encode(debug_hash_bytes);
         println!("Let's debug: {:?}", debug_hash_string);
-    }
-
-    #[test]
-    fn check_guess_solution() {
-        // Get Alice as an account ID
-        let alice = AccountId::new_unchecked("alice.testnet".to_string());
-        // Set up the testing context and unit test environment
-        let context = get_context(alice);
-        testing_env!(context.build());
-
-        // Set up contract object and call the new method
-        let mut contract = Contract::new(
-            "69c2feb084439956193f4c21936025f14a5a5a78979d67ae34762e18a7206a0f".to_string(),
-        );
-        let mut guess_result = contract.guess_solution("wrong answer here".to_string());
-        assert!(!guess_result, "Expected a failure from the wrong guess");
-        assert_eq!(get_logs(), ["Try again."], "Expected a failure log.");
-        guess_result = contract.guess_solution("near nomicon ref finance".to_string());
-        assert!(guess_result, "Expected the correct answer to return true.");
-        assert_eq!(
-            get_logs(),
-            ["Try again.", "You guessed right!"],
-            "Expected a successful log after the previous failed log."
-        );
     }
 }
